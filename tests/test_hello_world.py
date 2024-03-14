15a916d9a9c75235207037091715f2a65dc135b8
yago-mendoza <yagomendoza.dev@gmail.com>
2024-03-14 18:56:05 +0100

    first commit

---
+++ tests/test_hello_world.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestHelloWorld(TestCase):
+    def test_upper(self):
+        self.assertEqual("hello world!".upper(), "HELLO WORLD!")
+
+# run : poetry run pytest
