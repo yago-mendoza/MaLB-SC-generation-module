f0e7c2c224d534ddec3d90624300baa582e44344
yago-mendoza <yagomendoza.dev@gmail.com>
2024-05-16 18:47:52 +0200

    style(RP_dspy_module): finalized ipynb file except for M4 module

--- sketches/RP_module/test.py
+++ sketches/RP_module/test.py
@@ -1,10 +1,7 @@
 import tempfile
 from pathlib import Path
 
-def load_if_exists(dspy_module, name):
-    dir_tmp = Path(tempfile.gettempdir())
-    if (dir_tmp / f"{dspy_module.__name__}_{name}.json").exists():
-        dspy_module.load(name)
+owan 
 
-    return dspy_module
-
+loaded_optimized_cot_qa = CoT()
+loaded_optimized_cot_qa.load("file.json")
